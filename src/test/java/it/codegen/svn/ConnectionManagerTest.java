a56a5fbdac71b0399f96044135ca02ac4e977474
thilanka <thilanka@codegen.net>
2016-05-10 12:40:47 +0530

    Add svn kit

--- src/test/java/it/codegen/svn/ConnectionManagerTest.java
+++ src/test/java/it/codegen/svn/ConnectionManagerTest.java
@@ -2,13 +2,19 @@ package it.codegen.svn;
 
 import junit.framework.TestCase;
 import org.junit.Test;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.wc.ISVNOptions;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNDiffClient;
 import org.tmatesoft.svn.core.wc.SVNWCUtil;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Created by thilanka on 5/9/2016.
@@ -19,13 +25,17 @@ public class ConnectionManagerTest extends TestCase
     private static String name = "thilanka";
     private static String password = "thilanka321";
 
-    @Test
-    public void testConnection()
+    @Test public void testConnection()
     {
         try
         {
             SVNClientManager clientManager = getSVNClientManager();
             assertNotNull( clientManager );
+
+            SVNDiffClient diffClient = clientManager.getDiffClient();
+            //TODO
+
+
         }
         catch( Exception e )
         {
@@ -34,19 +44,48 @@ public class ConnectionManagerTest extends TestCase
         }
     }
 
-    public static SVNClientManager getSVNClientManager () throws Exception
+    public static SVNClientManager getSVNClientManager() throws Exception
     {
-        SVNURL url = SVNURL.parseURIEncoded(svnUrl);
-        SVNRepository repository = SVNRepositoryFactory.create(url, null);
-        ISVNOptions myOptions = SVNWCUtil.createDefaultOptions(true);
+        SVNURL url = SVNURL.parseURIEncoded( svnUrl );
+        SVNRepository repository = SVNRepositoryFactory.create( url, null );
+        ISVNOptions myOptions = SVNWCUtil.createDefaultOptions( true );
 
-        ISVNAuthenticationManager myAuthManager = SVNWCUtil
-                .createDefaultAuthenticationManager(name, password);
-        repository.setAuthenticationManager(myAuthManager);
+        ISVNAuthenticationManager myAuthManager = SVNWCUtil.createDefaultAuthenticationManager( name, password );
+        repository.setAuthenticationManager( myAuthManager );
 
-        SVNClientManager clientManager = SVNClientManager.newInstance(
-                myOptions, myAuthManager);
+        SVNClientManager clientManager = SVNClientManager.newInstance( myOptions, myAuthManager );
 
         return clientManager;
+    }
+
+    @Test
+    public void testRepository()
+    {
+        long startRevision = 528513;
+        long endRevision = 528515;
+
+        try
+        {
+            SVNRepository repository = SVNRepositoryFactory.create( SVNURL.parseURIEncoded( svnUrl ) );
+            ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager( name, password );
+            repository.setAuthenticationManager( authManager );
+            System.out.println("Latest Revision :" + repository.getLatestRevision());
+
+            Collection logEntries = repository.log( new String[]{""}, null, startRevision, endRevision, true, true );
+            for( Iterator entries = logEntries.iterator(); entries.hasNext(); )
+            {
+                SVNLogEntry logEntry = (SVNLogEntry) entries.next();
+                System.out.println( String.format( "revision: %d, date %s", logEntry.getRevision(), logEntry.getDate() ) );
+
+                logEntry.getChangedPaths().forEach( (k,v) -> System.out.println(v.getPath()) );
+                System.out.println("----------------------------");
+            }
+
+        }
+        catch( SVNException e )
+        {
+            e.printStackTrace();
+            fail();
+        }
     }
 }
